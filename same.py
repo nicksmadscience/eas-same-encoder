be387b24aaad6c32740a6e102aff6467d648f971
Nick Piegari <nickpiegari@gmail.com>
2025-04-02 15:50:59 -0400

    made into a class for best-practice purposes

--- same.py
+++ same.py
@@ -5,143 +5,151 @@ import argparse
 
 
 
+class SAME:
+	"""Generates valid EAS SAME messages and tones."""
 
-######## CONFIG / constants ########
+	def __init__(self):
+		######## CONFIG / constants ########
 
-samplerate = 43750 # makes for easy lowest common denominatorization
-
-
-
-
-
-def markBit():
-	f = 2083.33333
-	t = 1.0 / (520 + (5/6))
-	
-	samples = np.arange(t * samplerate) / samplerate
-
-	return np.sin(2 * np.pi * f * samples)
-
-
-
-def spaceBit():
-	f = 1562.5
-	t = 1.0 / (520 + (5/6))
-	
-	samples = np.arange(t * samplerate) / samplerate
-
-	return np.sin(2 * np.pi * f * samples)
+		self.samplerate = 43750 # makes for easy lowest common denominatorization
 
 
 
 
-def byte(the_byte):
-	byte_data = np.zeros(0)
-	for i in range(0, 8):
-		if ord(the_byte) >> i & 1:
-			byte_data = np.append(byte_data, markBit())
-		else:
-			byte_data = np.append(byte_data, spaceBit())
 
-	return byte_data
+	def markBit(self):
+		f = 2083.33333
+		t = 1.0 / (520 + (5/6))
+		
+		samples = np.arange(t * self.samplerate) / self.samplerate
 
-
-
-def preamble():
-	byte_data = np.zeros(0)
-
-	for i in range(0, 16):
-		byte_data = np.append(byte_data, markBit())
-		byte_data = np.append(byte_data, markBit())
-		byte_data = np.append(byte_data, spaceBit())
-		byte_data = np.append(byte_data, markBit())
-		byte_data = np.append(byte_data, spaceBit())
-		byte_data = np.append(byte_data, markBit())
-		byte_data = np.append(byte_data, spaceBit())
-		byte_data = np.append(byte_data, markBit())
-
-	return byte_data
+		return np.sin(2 * np.pi * f * samples)
 
 
 
-def attentiontone(length=5):
-	tone = np.sin(2 * np.pi * 853 * np.arange(0, length, 1/samplerate)) 
-	tone2 = np.sin(2 * np.pi * 960 * np.arange(0, length, 1/samplerate)) 
+	def spaceBit(self):
+		f = 1562.5
+		t = 1.0 / (520 + (5/6))
+		
+		samples = np.arange(t * self.samplerate) / self.samplerate
 
-	return (tone + tone2) / 2
-
-
-def noaatone(length=5):
-	return np.sin(2 * np.pi * 1050 * np.arange(0, length, 1/samplerate))
-
-
-def pause(length=1):
-	return np.zeros(int(samplerate * length))
+		return np.sin(2 * np.pi * f * samples)
 
 
 
 
-# EAS alerts are heavily dependent on timestamps so this makes it easy/fun to send a thing now
-sameCompatibleTimestamp = datetime.datetime.now().strftime("%j%H%M")
+	def byte(self, the_byte):
+		byte_data = np.zeros(0)
+		for i in range(0, 8):
+			if ord(the_byte) >> i & 1:
+				byte_data = np.append(byte_data, self.markBit())
+			else:
+				byte_data = np.append(byte_data, self.spaceBit())
 
-# known good
-code = "ZCZC-PEP-EAN-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -"  # nuclear armageddon (or some other form of "we are all likely to die")
-
-
-
-def buildMessage(code, filename="same.wav", attention="eas"):
-
-	# STEP ONE: Insert a bit of silence
-	signal = pause(0.5)
+		return byte_data
 
 
-	# message
-	for i in range(0, 3):
-		signal = np.append(signal, preamble())
 
-		# turn each character into a sequence of sine waves
-		for char in code:
-			signal = np.append(signal, byte(char))
+	def preamble(self):
+		byte_data = np.zeros(0)
 
-		signal = np.append(signal, pause(1)) # wait the requisite one second
+		for i in range(0, 16):
+			byte_data = np.append(byte_data, self.markBit())
+			byte_data = np.append(byte_data, self.markBit())
+			byte_data = np.append(byte_data, self.spaceBit())
+			byte_data = np.append(byte_data, self.markBit())
+			byte_data = np.append(byte_data, self.spaceBit())
+			byte_data = np.append(byte_data, self.markBit())
+			byte_data = np.append(byte_data, self.spaceBit())
+			byte_data = np.append(byte_data, self.markBit())
+
+		return byte_data
 
 
-	if attention == "eas":
-		signal = np.append(signal, attentiontone())
-		signal = np.append(signal, pause(1))
+
+	def attentiontone(self, length=5):
+		tone = np.sin(2 * np.pi * 853 * np.arange(0, length, 1/self.samplerate)) 
+		tone2 = np.sin(2 * np.pi * 960 * np.arange(0, length, 1/self.samplerate)) 
+
+		return (tone + tone2) / 2
+
+
+	def noaatone(self, length=5):
+		return np.sin(2 * np.pi * 1050 * np.arange(0, length, 1/self.samplerate))
+
+
+	def pause(self, length=1):
+		return np.zeros(int(self.samplerate * length))
+
+
+
 
 	
-	if attention == "noaa":
-		signal = np.append(signal, noaatone())
-		signal = np.append(signal, pause(1))
 
 
-	# EOM (3x)
-	for i in range(0, 3):
-		signal = np.append(signal, preamble())
 
-		for char in "NNNN": # NNNN = End Of Message
-			signal = np.append(signal, byte(char))
+	def buildMessage(self, code, filename="same.wav", attention="eas"):
 
-		signal = np.append(signal, pause(1)) # wait the requisite one second
+		# STEP ONE: Insert a bit of silence
+		signal = self.pause(0.5)
+
+
+		# message
+		for i in range(0, 3):
+			signal = np.append(signal, self.preamble())
+
+			# turn each character into a sequence of sine waves
+			for char in code:
+				signal = np.append(signal, self.byte(char))
+
+			signal = np.append(signal, self.pause(1)) # wait the requisite one second
+
+
+		if attention == "eas":
+			signal = np.append(signal, self.attentiontone())
+			signal = np.append(signal, self.pause(1))
 
 		
-	# wave-ify
-	signal *= 32767
+		if attention == "noaa":
+			signal = np.append(signal, self.noaatone())
+			signal = np.append(signal, self.pause(1))
 
-	signal = np.int16(signal)
 
-	scipy.io.wavfile.write(filename, samplerate, signal)
+		# EOM (3x)
+		for i in range(0, 3):
+			signal = np.append(signal, self.preamble())
+
+			for char in "NNNN": # NNNN = End Of Message
+				signal = np.append(signal, self.byte(char))
+
+			signal = np.append(signal, self.pause(1)) # wait the requisite one second
+
+			
+		# wave-ify
+		signal *= 26000 # max is 32767 but i want to trim it down just a little
+
+		signal = np.int16(signal)
+
+		scipy.io.wavfile.write(filename, self.samplerate, signal)
 
 
 
 
 
 if __name__ == "__main__":
+	# EAS alerts are heavily dependent on timestamps so this makes it easy/fun to send a thing now
+	sameCompatibleTimestamp = datetime.datetime.now().strftime("%j%H%M")
+
 	# parse command-line arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--code", "-c", nargs='?', default="none")
+	parser.add_argument("--code", "-c", nargs='?',
+					 default="ZCZC-PEP-EAN-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -")
+	parser.add_argument("--tone", "-t", default="eas")
 	args = parser.parse_args()
 
-	buildMessage(code)
+	print (args.code)
+
+	same = SAME()
+
+	same.buildMessage(args.code)
 
