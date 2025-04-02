7399b5aba2ed527b61867d643c49da503c93c8f4
Nick Piegari <nickpiegari@gmail.com>
2025-04-02 17:18:25 -0400

    descriptions, refactoring

--- same.py
+++ same.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+"""\
+Generates valid EAS SAME messages and tones.
+
+Usage: python3 same.py --code=(code) --filename=(filename) 
+"""
+__author__ = "Nick Piegari"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "nickpiegari@gmail.com"
+
+
 import numpy as np
 import scipy.io.wavfile
 import datetime # EAS alerts are heavily dependent on timestamps so this makes it easy to send a thing now
@@ -11,13 +23,11 @@ class SAME:
 	def __init__(self):
 		######## CONFIG / constants ########
 
-		self.samplerate = 43750 # makes for easy lowest common denominatorization
-
-
-
+		self.samplerate = 43750 # makes for easy lowest common denominatorization.  not tested at any other rate
 
 
 	def markBit(self):
+		"""Generates a mark bit, four cycles of 2083 1/3 Hz."""
 		f = 2083.33333
 		t = 1.0 / (520 + (5/6))
 		
@@ -26,8 +36,8 @@ class SAME:
 		return np.sin(2 * np.pi * f * samples)
 
 
-
 	def spaceBit(self):
+		"""Generates a space bit, four cycles of 1562.5Hz."""
 		f = 1562.5
 		t = 1.0 / (520 + (5/6))
 		
@@ -36,9 +46,8 @@ class SAME:
 		return np.sin(2 * np.pi * f * samples)
 
 
-
-
 	def byte(self, the_byte):
+		"""Turns the provided byte into FSK data."""
 		byte_data = np.zeros(0)
 		for i in range(0, 8):
 			if ord(the_byte) >> i & 1:
@@ -49,8 +58,8 @@ class SAME:
 		return byte_data
 
 
-
 	def preamble(self):
+		"""Builds the sixteen-byte 10101011 preamble."""
 		byte_data = np.zeros(0)
 
 		for i in range(0, 16):
@@ -66,8 +75,8 @@ class SAME:
 		return byte_data
 
 
-
 	def attentiontone(self, length=5):
+		"""Generates a standard two-tone attention signal."""
 		tone = np.sin(2 * np.pi * 853 * np.arange(0, length, 1/self.samplerate)) 
 		tone2 = np.sin(2 * np.pi * 960 * np.arange(0, length, 1/self.samplerate)) 
 
@@ -75,26 +84,23 @@ class SAME:
 
 
 	def noaatone(self, length=5):
+		"""Generates a weather-radio-style 1050Hz tone."""
 		return np.sin(2 * np.pi * 1050 * np.arange(0, length, 1/self.samplerate))
 
 
 	def pause(self, length=1):
+		"""Generates a period of silence for the specified duration."""
 		return np.zeros(int(self.samplerate * length))
 
 
-
-
-	
-
-
-
 	def buildMessage(self, code, filename="same.wav", tone="eas", db=-3.0):
+		"""Generates and writes to file a complete EAS SAME message.."""
 
 		# STEP ONE: Insert a bit of silence
 		signal = self.pause(0.5)
 
 
-		# message
+		# message (3x)
 		for i in range(0, 3):
 			signal = np.append(signal, self.preamble())
 
@@ -105,6 +111,7 @@ class SAME:
 			signal = np.append(signal, self.pause(1)) # wait the requisite one second
 
 
+		# tone.  will be skipped if it's not 'eas' or 'tone'
 		if tone == "eas":
 			signal = np.append(signal, self.attentiontone())
 			signal = np.append(signal, self.pause(1))
@@ -122,7 +129,8 @@ class SAME:
 
 			signal = np.append(signal, self.pause(1)) # wait the requisite one second
 
-		# wave-ify
+
+		# wave-ify and adjust trim (decibel to normal conversion)
 		signal *= 32767 * (10.0 ** (db / 20.0))
 
 		signal = np.int16(signal)
@@ -139,11 +147,12 @@ if __name__ == "__main__":
 
 	# parse command-line arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--code", "-c", nargs='?',
-					 default="ZCZC-PEP-EAN-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -")
-	parser.add_argument("--filename", "-f", default="same.wav")
-	parser.add_argument("--tone", "-t", default="eas")
-	parser.add_argument("--db", "-d", default=-3.0)
+	parser.add_argument("--code", "-c", nargs='?',  
+					 default="ZCZC-EAS-RWT-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -",
+					 help="The message to be SAME-ified.  Can be anything, but only valid messages will be decoded by an EAS device.")
+	parser.add_argument("--filename", "-f", default="same.wav", help="Writes to this file.")
+	parser.add_argument("--tone", "-t", default="eas", help="Whether to use EAS tone, NOAA tone, or no tone.")
+	parser.add_argument("--db", "-d", default=-3.0, help="Trim adjustment in decibels.  Anything over 0dB will distort and will probably not decode correctly.")
 	args = parser.parse_args()
 
 	print (args.code)
