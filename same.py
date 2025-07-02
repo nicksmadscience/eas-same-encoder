36f46b00cd007614fac154f83aae84e2b9dd3c30
Nick Piegari <nickpiegari@gmail.com>
2025-07-02 15:33:16 -0400

    hello

--- same.py
+++ same.py
@@ -84,7 +84,7 @@ class SAME:
 
 
 	def noaatone(self, length=5):
-		"""Generates a weather-radio-style 1050Hz tone."""
+		"""Generates a weather-radio-style 1050Hz tone..........."""
 		return np.sin(2 * np.pi * 1050 * np.arange(0, length, 1/self.samplerate))
 
 
